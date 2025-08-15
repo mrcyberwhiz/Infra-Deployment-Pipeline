ea7e5096b9c4c40e6392973a58d9fb8155c4836b
DHANUSH S <99108461+mrcyberwhiz@users.noreply.github.com>
2025-08-15 11:43:49 +0530

    Update backend.tf

--- environments/qa/backend.tf
+++ environments/qa/backend.tf
@@ -7,7 +7,7 @@ terraform {
   }
   backend "s3" {
     bucket          = "terraform-bucket-infradeployment"
-    key             = "environments/dev/terraform.tfstate"
+    key             = "environments/qa/terraform.tfstate"
     region          = "ap-south-1"
     dynamodb_table  = "terraform-lock"
 
